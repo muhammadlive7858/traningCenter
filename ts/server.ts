76dcd7fd855444f3daabdbe1c4a1598640a1d41f
Zokirbek <zyusupov51@mail.com>
2022-06-16 21:42:50 +0500

    thirtCommit

--- ts/server.ts
+++ ts/server.ts
@@ -1,9 +1,16 @@
-import  Express  from "express";
+import  express  from "express";
+import cors from 'cors';
 
-const app = Express()
+const app = express();
+
+// core middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// server is building
 const port = 7777;
-
-
 
 app.listen(port,()=>{
     console.log("server running 7777");
